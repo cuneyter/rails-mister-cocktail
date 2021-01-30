4864a73efc2c89f8305d8eae503de3ca36ee2927
Cuneyt Ergun <cuneytergun@gmail.com>
2021-01-30 21:41:55 +0000

    add association in the new dose form, add list of dose and ingredient in the cocktail show page

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -6,7 +6,7 @@ class DosesController < ApplicationController
 
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
-    @cocktail = Dose.new(dose_params)
+    @dose = Dose.new(dose_params)
     # we need `restaurant_id` to associate review with corresponding restaurant
     @dose.cocktail = @cocktail
     if @dose.save
@@ -22,6 +22,6 @@ class DosesController < ApplicationController
   private
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
   end
 end
