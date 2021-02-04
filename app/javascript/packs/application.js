7c71fa55fa9e20e17fc7d5cfd00f31b977db9049
Cuneyt Ergun <cuneytergun@gmail.com>
2021-02-04 20:10:32 +0000

    add card for each cocktail and style it

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,3 +12,15 @@ import 'bootstrap';
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener('turbolinks:load', () => {
+  const nav = document.querySelector('nav');
+
+  window.addEventListener('scroll', function () {
+    if (window.pageYOffset > 100) {
+      nav.classList.add('bg-dark', 'shadow');
+    } else {
+      nav.classList.remove('bg-dark', 'shadow');
+    }
+  });
+})
